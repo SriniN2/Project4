0f9861e02d1e35145e189202b30b1b3a69aff086
SriniN2 <nivasrin2@gmail.com>
2022-09-08 11:36:15 +0530

    Project Structure

---
+++ src/org/test/Panjayathu.java
@@ -0,0 +1,12 @@
+package org.test;
+
+public class Panjayathu {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Vettu");
+		System.out.println("Aruvaa");
+		System.out.println("Sandaaa");
+		
+	}
+}
